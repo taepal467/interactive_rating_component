39143a8914a0d8711ab79b1f2d04f701420dbc5f
Chantae P <60330899+taepal467@users.noreply.github.com>
2022-04-09 14:31:54 -0700

    updated script file
    I changed my code in the script file to use jQuery

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-let getAllRatingButtons = document.querySelectorAll(".rating-btn");
-let buttonOne = document.getElementsByClassName("one")[0];
-let buttonTwo = document.getElementsByClassName("two")[0];
-let buttonThree = document.getElementsByClassName("three")[0];
-let buttonFour = document.getElementsByClassName("four")[0];
-let buttonFive = document.getElementsByClassName("five")[0];
-let submitButton = document.querySelector(".submit-btn");
+
+const getForm = document.getElementById("rate-form");
+
+getForm.addEventListener("submit", getRating);
+
+function getRating(event) {
+  event.preventDefault();
+
+  const ratings = document.querySelector('input[name="rating"]:checked').value;
+  
+  $('#submit-button').on("click", function() {
+
+    $("#thank-you-content").toggle();
+    $("#rating-content").toggle();
+    $(".rate-content").html(`You selected ${ratings} out of 5`);
+    setTimeout(function(){
+      history.back();
+    }, 1000);
+   
+   })
+}
 
 
 
-let buttonArray = [
-  buttonOne,
-  buttonTwo,
-  buttonThree,
-  buttonFour,
-  buttonFive
-]
 
-submitButton.addEventListener("click", function() {
-  for (let i = 0; i < buttonArray.length; i++) {
-    if(buttonArray[0]) {
-      document.getElementById("thank-you-content").style.display = "block";
-      document.getElementById("rating-content").style.display = "none";
-      document.getElementsByClassName("rate-content")[0].innerHTML = "You selected 1 out of 5";
-    } else if (buttonArray[1]) {
-      document.getElementById("thank-you-content").style.display = "block";
-      document.getElementById("rating-content").style.display = "none";
-      document.getElementsByClassName("rate-content")[0].innerHTML = "You selected 2 out of 5";
-    }
-  }
-}); 
+
 
 
 
